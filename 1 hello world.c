ea61132b620ecc6bf5c01f7da1c87948c4d9738d
AshishShroti10260 <ashishshroti10260@gmail.com>
2024-07-09 18:53:42 +0530

    Add files via upload

---
+++ 1 hello world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main ()
+{ int xkm ;
+    scanf ( "enter the value of km" ,xkm);
+    scanf("%d",&xkm);
+    printf("so the value of kilometer in meter is %d",1000*xkm );
+}
